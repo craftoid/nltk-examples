44d6b871073e8c0a91530f43defd38cb43de32e2
Martin Schneider <craftoid@k2g2.org>
2015-06-17 17:17:34 +0200

    densify

--- syllable/syllable.py
+++ syllable/syllable.py
@@ -4,19 +4,13 @@ cmu = nltk.corpus.cmudict.dict()
 
 def count_syllables(word):
 	""" How many syllables are there in a word? """
-	
 	vowelcounts = []
-
 	for pronounciation in cmu[word]:
-
 		vowels = []
-
 		for phoneme in pronounciation:
 			if phoneme[-1].isdigit():
 				vowels.append(phoneme)
-
 		vowelcounts.append(len(vowels))
-	
 	return max(vowelcounts)
 
 
